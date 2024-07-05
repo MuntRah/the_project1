a04d1fc59968716e04ded2a84b36702ad795b887
MuntRah <me.alrahim.02@gmail.com>
2024-07-05 21:10:33 +0300

    the forth ver

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
+let idx;
+let letter;
+let currentWord;
 const words = [
-  { word: "dog", hint: "Man's best friend" },
-  { word: "tree", hint: "Has leaves and branches" },
-  { word: "sun", hint: "The center of our solar system" },
-  { word: "book", hint: "Contains stories or information" },
-  { word: "ball", hint: "Round and used in sports" },
-  { word: "cat", hint: "A common household pet" },
-  { word: "house", hint: "Where people live" },
-  { word: "car", hint: "Used for transportation on roads " },
-  { word: "rain", hint: "Falls from the sky in drops" },
-  { word: "bird", hint: "Has feathers and flies" },
-  { word: "elephant", hint: "A large mammal with tusks and a trun" },
-  { word: "computer", hint: "A device used for processing informatio" },
-  { word: "mountain", hint: "A large natural elevation of the earth's surfa " },
-  { word: "oxygen", hint: "A chemical element essential for respirati " },
-  { word: "guitar", hint: "A musical instrument with string" },
+  { word: "DOG", hint: "Humens best friend" },
+  { word: "TREE", hint: "Has leaves and branches" },
+  { word: "SUN", hint: "The center of our solar system" },
+  { word: "BOOK", hint: "Contains stories or information" },
+  { word: "BALL", hint: "Round and used in sports" },
+  { word: "CAT", hint: "A common household pet" },
+  { word: "HOUSE", hint: "Where people live" },
+  { word: "CAR", hint: "Used for transportation on roads" },
+  { word: "RAIN", hint: "Falls from the sky in drops" },
+  { word: "BIRD", hint: "Has feathers and flies" },
+  { word: "ELEPHANT", hint: "A large mammal with tusks and a trunk" },
+  { word: "COMPUTER", hint: "A device used for processing information" },
+  {
+    word: "MOUNTAIN",
+    hint: "A large natural elevation of the earth's surface",
+  },
+  { word: "OXYGEN", hint: "A chemical element essential for respiration" },
+  { word: "GUITAR", hint: "A musical instrument with strings" },
 ];
+const wrongDisplay = document.querySelector(".wrong");
+const wordDisplay = document.querySelector(".word");
+const guessDisplay = document.querySelector(".guess");
+const hintDisplay = document.querySelector(".hint");
+const letterButtons = document.querySelectorAll(".keyboard button");
 
-  const guessDisplay = document.querySelector(".guess");
-  const hintDisplay = document.querySelector(".hint");
-  const letterButtons = document.querySelectorAll(".keyboard button");
-
-  letterButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const letter = button.innerText;
-      guessDisplay.innerText += letter;
-    });
+letterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const letter = button.innerText;
+    btn(letter);
   });
+});
+// words.forEach((n) => {
+//   word.innerHTML = n.word;
+//   hintDisplay.innerText = `hint : ${n.hint}`;
+// });
 
+function rand() {
+  idx = Math.floor(Math.random() * words.length);
+  currentWord = words[idx].word;
+  wordDisplay.innerText = `${words[idx].word}`;
+  hintDisplay.innerText = `hint :${words[idx].hint}`;
+}
+document.addEventListener("DOMContentLoaded", rand);
+
+function btn(str) {
+  let found = false;
+  for (let index = 0; index < currentWord.length; index++) {
+    if (currentWord[index] === str) {
+      let wordIndex = guessDisplay.innerText.split("");
+      wordIndex[index] = str;
+      guessDisplay.innerHTML = wordIndex.join("");
+      found = true;
+    }
+  }
+
+  if (!found) {
+    wrongDisplay.innerText += str;
+    letterButtons.forEach((button) => {
+      if (button.innerText === str) {
+        button.disabled = true;
+      }
+    });
+  }
+}
